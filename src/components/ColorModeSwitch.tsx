8d731c7e79f2fdeed4cf1674edc7c322b0c389c3
Nidhi Patel <nidhiajol12@gmail.com>
2025-03-04 12:23:48 -0500

    DarkMode button

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,38 @@
+import { HStack, Switch, Icon } from "@chakra-ui/react";
+import { useColorMode } from "@/components/ui/color-mode";
+import { FaMoon, FaSun } from "react-icons/fa";
+
+const ColorModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <div>
+      <HStack>
+        {/* <Switch isChecked={colorMode === "dark"} onChange={toggleColorMode} /> */}
+        {/* <Switch.Root>
+          <Switch.HiddenInput />
+          <Switch.Control onClick={toggleColorMode}>
+            <Switch.Thumb />
+          </Switch.Control>
+          <Switch.Label>
+            {colorMode === "light" ? "🌙 Dark Mode" : "☀️ Light Mode"}
+          </Switch.Label>
+        </Switch.Root> */}
+
+        <Switch.Root colorPalette="blue" size="lg">
+          <Switch.HiddenInput />
+          <Switch.Control onClick={toggleColorMode}>
+            <Switch.Thumb />
+            <Switch.Indicator fallback={<Icon as={FaMoon} color="gray.400" />}>
+              <Icon as={FaSun} color="yellow.400" />
+            </Switch.Indicator>
+          </Switch.Control>
+          <Switch.Label>
+            {colorMode === "light" ? "Dark Mode" : "Light Mode"}
+          </Switch.Label>
+        </Switch.Root>
+      </HStack>
+    </div>
+  );
+};
+
+export default ColorModeSwitch;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,18 @@
-import { HStack, Text } from "@chakra-ui/react"
+import { HStack, Text} from "@chakra-ui/react"
 import logo from '../assets/react.svg'
 import { Image } from "@chakra-ui/react"
-
+import ColorModeSwitch from "./ColorModeSwitch"
 
 const Navbar = () => {
+    
   return (
     <div>
-      <HStack>
+      <HStack justifyContent='space-between' padding='10px'>
+        <HStack>
         <Image src={logo} boxSize='40px' />
-        <Text>Game Hub</Text>
+        <Text fontSize='2xl' fontWeight='bold'>Game Hub</Text>
+        </HStack>
+        <ColorModeSwitch />
         </HStack>
     </div>
   )
