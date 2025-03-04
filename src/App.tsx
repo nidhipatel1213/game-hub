e2f5d915d63fb60155aad7e7c7feec676babe3da
Nidhi Patel <nidhiajol12@gmail.com>
2025-03-04 11:08:55 -0500

    Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { Button } from "@chakra-ui/react"
+import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
 
 function App() {
-  
+  const showAside = useBreakpointValue({ base: false, lg: true });
 
   return (
     <>
-      <Button>Click me</Button>
+      <Grid templateAreas={{
+        base: `"nav"  "main"`,
+        lg: `"nav nav"  "aside main"`, //1024 px
+      }}>
+        <GridItem area="nav" bg="coral">
+          <h1>Nav</h1>
+        </GridItem>
+        {showAside && (
+          <GridItem area="aside" bg="pink">
+            <h1>Aside</h1>
+          </GridItem>
+        )}
+        <GridItem area="main" bg="orange">
+          <h1>Main</h1>
+        </GridItem>
+      </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
